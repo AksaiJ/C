a6db929b11690e0a13dde496e03d2610d91df2ab
AkshayJDell <akshaytesting88@gmail.com>
2022-01-03 16:57:26 +0530

    pass pointer string param, return doub poin and assign to a new pointer

---
+++ strings/stringsIntro.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+
+    char ** tooggleCase(char **charray){
+        int i;
+        printf("%s",charray);
+        for(i = 0; charray[i] != '\0'; i++){
+            if (charray[i] >= 65 && charray[i] <=90){
+                charray[i] = charray[i] + 32;
+            }
+            else if (charray[i] >= 'a' && charray[i] <= 'z'){
+                charray[i] = charray[i] - 32;
+            }
+        }
+        return charray;
+
+    }   
+
+int main (){
+
+
+    char temp;
+    temp = 'A';
+    printf("%c",temp);
+
+
+
+     char X[5] = {'A', 'B', 'C', 'D', 'E',};
+
+     char x1[5] = {'65', '66', '67', '68', '69'};
+
+     char *chararray ="hello";
+
+     char welc[] = "welcome";
+
+     char *sarcasmCase = "WhAtAre";
+
+     printf("%s",welc);
+    int i;
+     //change case
+    for(i = 0; welc[i]!= '\0'; i++){
+        welc[i] = welc[i] - 32;
+    }
+
+    char *sarcasmCase2 = tooggleCase(sarcasmCase);
+
+    printf("%u %u \n", &welc, &welc[1]);
+
+    printf("%s sarcasm 1 \n",sarcasmCase);
+
+    printf("%s sarcasmCase2 \n", sarcasmCase2);
+
+
+     
+
+    
+
+    return 1;
+}
